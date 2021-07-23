54dd8a36f66028ac8b3a7153590e452177bfe8bc
Raymond <raymond@philippinedev.com>
2021-07-23 12:59:41 +0800

    save functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,23 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new
+
+    if @contact.save
+      redirect_to contacts_path, notice: "Contact successfully added"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+  end
 end
